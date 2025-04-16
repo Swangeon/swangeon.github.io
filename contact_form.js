724527678ca3407ac8b1ab68cb693c6524716fab
swangeon <swangeon@gmail.com>
2025-04-15 22:44:15 -0500

    style: Change the font color for the result text on the contact form
    It depends now on if it was success (green text) or an error occurred (red text) instead of it always being green text

--- contact_form.js
+++ contact_form.js
@@ -12,6 +12,7 @@ async function handleSubmit(event) {
     }
   }).then(response => {
     if (response.ok) {
+      status.style.color = "green";
       status.innerHTML = "Thanks for your submission!";
       form.reset()
     } else {
@@ -19,11 +20,13 @@ async function handleSubmit(event) {
         if (Object.hasOwn(data, 'errors')) {
           status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
         } else {
+          status.style.color = "red";
           status.innerHTML = "Oops! There was a problem submitting your form"
         }
       })
     }
   }).catch(error => {
+    status.style.color = "red";
     status.innerHTML = "Oops! There was a problem submitting your form"
   });
 }
